990874f4695eb2a5dbeb6c07659ef882c41ad645
njmarko <marko.njegomir@uns.ac.rs>
2022-08-05 22:49:24 +0000

    Added new py files for nightly testing. Jupyter was run on CIFAR100
    Former-commit-id: 8b1a30f0640c2a3f5ff8b57489ade591f000193f

--- GATTransformer.py
+++ GATTransformer.py
@@ -3,7 +3,7 @@
 
 # ## Custom GNN Transformer architecture
 
-# In[51]:
+# In[50]:
 
 
 import torch
@@ -12,12 +12,17 @@ import time
 from torch import optim
 import torch.nn.functional as F
 import torch_geometric
-from torch_geometric.nn import GATConv
+from torch_geometric.nn import GATConv, GATv2Conv
 import torchvision
 from einops import rearrange
+import wandb
+from sklearn import metrics 
+import os
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-# In[92]:
+# In[51]:
 
 
 class Residual(nn.Module):
@@ -48,9 +53,20 @@ class FeedForward(nn.Module):
 
     def forward(self, x, **kwargs):
         return self.net(x)
+    
+class QuickFix(nn.Module):
+    def __init__(self, dim, heads, fn):
+        super().__init__()
+        self.dim = dim
+        self.heads = heads
+        self.linear = nn.Linear(dim * heads, dim)
+        self.fn = fn
+        
+    def forward(self, x, **kwargs):
+        return self.linear(self.fn(x, **kwargs))
 
 
-# In[111]:
+# In[52]:
 
 
 # Out implementation of GAT Transformer
@@ -62,7 +78,7 @@ class GATTransformer(torch.nn.Module):
         self.depth = depth
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                Residual(PreNorm(dim, GATConv(in_channels=dim, out_channels=dim, heads=heads, add_self_loops=False))),
+                Residual(PreNorm(dim, QuickFix(dim, heads, GATv2Conv(in_channels=dim, out_channels=dim, heads=heads, add_self_loops=False)))),
                 Residual(PreNorm(dim, FeedForward(dim, mlp_dim)))
             ]))
 
@@ -77,7 +93,7 @@ class GATTransformer(torch.nn.Module):
         return x
 
 
-# In[112]:
+# In[53]:
 
 
 class GraphViT(nn.Module):
@@ -108,77 +124,106 @@ class GraphViT(nn.Module):
         p = self.patch_size
 
         x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p, p2 = p)
+        x = x.to(device)
         x = self.patch_to_embedding(x)
         
         cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
+        cls_tokens = cls_tokens.to(device)
         x = torch.cat((cls_tokens, x), dim=1)
         x += self.pos_embedding
         
-        # Adjacency matrix
-#         edge_index = torch.ones((self.num_patches+1,self.num_patches+1), dtype=torch.long))
+    
+        x_shape = x.shape
+        edge_index = torch.ones((2, x.shape[1]**2*x.shape[0]), dtype=torch.long)
+        for i in range(x.shape[0]*x.shape[1]):
+            for j in range(x.shape[1]):
+                edge_index[0,i*x.shape[1] + j] = i
+                edge_index[1,i*x.shape[1] + j] = x.shape[1]*(i // x.shape[1]) + j
+        x = x.view((x.shape[0]*x.shape[1], x.shape[2]))
+        edge_index = edge_index.to(device)
 
-        # Adjacency list
-        edge_index = torch.ones((2, (self.num_patches+1)**2), dtype=torch.long)
-        for i in range(self.num_patches + 1):
-            for j in range(self.num_patches + 1):
-                edge_index[0,i*(self.num_patches + 1) + j] = i
-                edge_index[1,i*(self.num_patches + 1) + j] = j
-
-        # squeeze unsueeze for batchsize 1 untill dataloader with batches is implemented
-        x = self.transformer([x.squeeze(), edge_index])
-        x = x.unsqueeze(0)
+        
+        x = self.transformer([x, edge_index])
+        x = x.view(x_shape)
         
         x = self.to_cls_token(x[:,0])
+        x = x.to(device)
+        
         out = self.mlp_head(x)
+        
         return out
 
 
-# In[113]:
+# In[54]:
 
 
 torch.manual_seed(42)
+# wandb.init(entity = 'njmarko', project = 'gat')
 
 DOWNLOAD_PATH = '/data/mnist'
-BATCH_SIZE_TRAIN = 1
-BATCH_SIZE_TEST = 1
+BATCH_SIZE_TRAIN = 100
+BATCH_SIZE_TEST = 1000
 
 transform_mnist = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
                                torchvision.transforms.Normalize((0.1307,), (0.3081,))])
-
-#train_set = torchvision.datasets.MNIST(DOWNLOAD_PATH, train=True, download=True,
-#                                       transform=transform_mnist)
-train_set = torchvision.datasets.MNIST(DOWNLOAD_PATH, train=True, download=True,
+train_set = torchvision.datasets.CIFAR100(DOWNLOAD_PATH, train=True, download=True,
                                        transform=transform_mnist)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True)
 
-test_set = torchvision.datasets.MNIST(DOWNLOAD_PATH, train=False, download=True,
+test_set = torchvision.datasets.CIFAR100(DOWNLOAD_PATH, train=False, download=True,
                                       transform=transform_mnist)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE_TEST, shuffle=True)
-# test_loader = torch_geometric.loader.DataLoader(test_set, batch_size=BATCH_SIZE_TEST, shuffle=True)
 
 
-# In[114]:
+# In[66]:
 
 
 def train_epoch(model, optimizer, data_loader, loss_history):
     total_samples = len(data_loader.dataset)
     model.train()
+    running_loss = 0.0
 
+    correct_samples = 0
     for i, (data, target) in enumerate(data_loader):
         optimizer.zero_grad()
         output = F.log_softmax(model(data), dim=1)
+        target = target.to(device)
+        output = output.to(device)
+        
+        _, pred = torch.max(output, dim=1)
+        
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        running_loss += loss.item()
+        
+        correct_samples += pred.eq(target).sum()
+        target = target.cpu().detach().numpy()
+        pred = pred.cpu().detach().numpy()
+        
+#         print(output)
+#         print('trarget')
+#         print(target)
+#         print('pred')
+#         print(pred)
+        
+        f1_score = metrics.f1_score(target, pred, average='micro')
 
         if i % 100 == 0:
             print('[' +  '{:5}'.format(i * len(data)) + '/' + '{:5}'.format(total_samples) +
                   ' (' + '{:3.0f}'.format(100 * i / len(data_loader)) + '%)]  Loss: ' +
                   '{:6.4f}'.format(loss.item()))
             loss_history.append(loss.item())
+            wandb.log({
+                'train_loss': loss.item(),
+                'train_f1_score': f1_score
+            })
+            
+    acc = 100.0 * correct_samples / total_samples
+    print(f'Accuracy: ' + '{:4.2f}'.format(acc) + '%')
 
 
-# In[115]:
+# In[67]:
 
 
 def evaluate(model, data_loader, loss_history):
@@ -190,47 +235,58 @@ def evaluate(model, data_loader, loss_history):
 
     with torch.no_grad():
         for data, target in data_loader:
-            output = F.log_softmax(model(data), dim=1)
+            res = model(data)
+            res = res.to(device)
+            output = F.log_softmax(res, dim=1)
+            target = target.to(device)
+            output = output.to(device)
             loss = F.nll_loss(output, target, reduction='sum')
             _, pred = torch.max(output, dim=1)
-            
+                        
             total_loss += loss.item()
             correct_samples += pred.eq(target).sum()
 
     avg_loss = total_loss / total_samples
+    acc = 100.0 * correct_samples / total_samples
     loss_history.append(avg_loss)
+    
+    target = target.cpu().detach().numpy()
+    pred = pred.cpu().detach().numpy()
+    f1_score = metrics.f1_score(target, pred, average='micro')
+    
+    wandb.log({
+        'test_loss': loss.item(),
+        'test_f1_score': f1_score
+    })
     print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
           '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
           '{:5}'.format(total_samples) + ' (' +
-          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
+          '{:4.2f}'.format(acc) + '%)\n')
 
 
-# In[116]:
+dim = [96, 96, 128]
+lr = [0.03, 0.003, 0.003]
+for i in range(3):
+    wandb.init(entity='njmarko', project=f'gat{i}')
+    N_EPOCHS = 40
 
+    start_time = time.time()
+    model = GraphViT(image_size=32, patch_size=8, num_classes=100, channels=3,
+                     dim=64, depth=6, heads=8, mlp_dim=128)
+    optimizer = optim.Adam(model.parameters(), lr=0.003)
+    model = model.to(device)
 
-N_EPOCHS = 25
+    # learning rate decay
+    train_loss_history, test_loss_history = [], []
+    for epoch in range(1, N_EPOCHS + 1):
+        print('Epoch:', epoch)
+        train_epoch(model, optimizer, train_loader, train_loss_history)
+        evaluate(model, test_loader, test_loss_history)
+        torch.save(model.state_dict(), os.path.join('models', f'train-{i}-{epoch}'))
+        # To do: early stopping
+        # if len(train_loss_history) > 2 and np.isclose(train_loss_history[-2], train_loss_history[-1], atol=10e-3): break
 
-start_time = time.time()
-model = GraphViT(image_size=28, patch_size=7, num_classes=10, channels=1,
-            dim=64, depth=4, heads=1, mlp_dim=64)
-optimizer = optim.Adam(model.parameters(), lr=0.003)
-
-train_loss_history, test_loss_history = [], []
-for epoch in range(1, N_EPOCHS + 1):
-    print('Epoch:', epoch)
-    train_epoch(model, optimizer, train_loader, train_loss_history)
-    evaluate(model, test_loader, test_loss_history)
-
-print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+    print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
 
 
 
