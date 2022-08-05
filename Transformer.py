68101118b87b9455b5e83649a3ed40a98b543f7b
njmarko <marko.njegomir@uns.ac.rs>
2022-08-05 17:23:02 +0000

    Added transformer python file.

---
+++ Transformer.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Transformer
+
+# Authors:
+# - Marina Debogović
+# - Marko Njegomir
+
+# ## VIT transformer implemented in pytorch that is used as a baseline for comparison
+# 
+# [Code for regular implementation of VIT transformer in pytorch](https://towardsdatascience.com/a-demonstration-of-using-vision-transformers-in-pytorch-mnist-handwritten-digit-recognition-407eafbc15b0)
+
+# In[3]:
+
+
+import torch
+from torch import nn
+import time
+from torch import optim
+
+torch.cuda.get_device_name(0)
+
+
+# In[4]:
+
+
+import torch
+import torch.nn.functional as F
+
+from torch import nn
+from einops import rearrange
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim ** -0.5
+
+        self.to_qkv = nn.Linear(dim, dim * 3, bias=False)
+        self.to_out = nn.Linear(dim, dim)
+
+    def forward(self, x, mask = None):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x)
+        q, k, v = rearrange(qkv, 'b n (qkv h d) -> qkv b h n d', qkv=3, h=h)
+
+        dots = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        # matricno mnozenje q i k
+
+        if mask is not None:
+            mask = F.pad(mask.flatten(1), (1, 0), value = True)
+            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
+            mask = mask[:, None, :] * mask[:, :, None]
+            dots.masked_fill_(~mask, float('-inf'))
+            del mask
+
+        attn = dots.softmax(dim=-1)
+
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out =  self.to_out(out)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, mlp_dim):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Residual(PreNorm(dim, Attention(dim, heads = heads))),
+                Residual(PreNorm(dim, FeedForward(dim, mlp_dim)))
+            ]))
+
+    def forward(self, x, mask=None):
+        for attn, ff in self.layers:
+            x = attn(x, mask=mask)
+            x = ff(x)
+        return x
+
+class ViT(nn.Module):
+    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, channels=3):
+        super().__init__()
+        assert image_size % patch_size == 0, 'image dimensions must be divisible by the patch size'
+        num_patches = (image_size // patch_size) ** 2
+        patch_dim = channels * patch_size ** 2
+
+        self.patch_size = patch_size
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
+        self.patch_to_embedding = nn.Linear(patch_dim, dim)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.transformer = Transformer(dim, depth, heads, mlp_dim)
+
+        self.to_cls_token = nn.Identity()
+
+        self.mlp_head = nn.Sequential(
+            nn.Linear(dim, mlp_dim),
+            nn.GELU(),
+            nn.Linear(mlp_dim, num_classes)
+        )
+
+    def forward(self, img, mask=None):
+        p = self.patch_size
+
+        x = rearrange(img, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = p, p2 = p)
+        x = self.patch_to_embedding(x)
+
+        cls_tokens = self.cls_token.expand(img.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x += self.pos_embedding
+        x = self.transformer(x, mask)
+
+        x = self.to_cls_token(x[:, 0])
+#         print(x.shape)
+        return self.mlp_head(x)
+
+
+# In[5]:
+
+
+import torch
+import torchvision
+
+torch.manual_seed(42)
+
+DOWNLOAD_PATH = '/data/mnist'
+BATCH_SIZE_TRAIN = 100
+BATCH_SIZE_TEST = 1000
+
+transform_mnist = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+
+train_set = torchvision.datasets.MNIST(DOWNLOAD_PATH, train=True, download=True,
+                                       transform=transform_mnist)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE_TRAIN, shuffle=True)
+
+test_set = torchvision.datasets.MNIST(DOWNLOAD_PATH, train=False, download=True,
+                                      transform=transform_mnist)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE_TEST, shuffle=True)
+
+
+# In[6]:
+
+
+def train_epoch(model, optimizer, data_loader, loss_history):
+    total_samples = len(data_loader.dataset)
+    model.train()
+
+    for i, (data, target) in enumerate(data_loader):
+        optimizer.zero_grad()
+        output = F.log_softmax(model(data), dim=1)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        if i % 100 == 0:
+            print('[' +  '{:5}'.format(i * len(data)) + '/' + '{:5}'.format(total_samples) +
+                  ' (' + '{:3.0f}'.format(100 * i / len(data_loader)) + '%)]  Loss: ' +
+                  '{:6.4f}'.format(loss.item()))
+            loss_history.append(loss.item())
+
+
+# In[7]:
+
+
+def evaluate(model, data_loader, loss_history):
+    model.eval()
+    
+    total_samples = len(data_loader.dataset)
+    correct_samples = 0
+    total_loss = 0
+
+    with torch.no_grad():
+        for data, target in data_loader:
+            output = F.log_softmx(model(data), dim=1)
+            loss = F.nll_loss(output, target, reduction='sum')
+            _, pred = torch.max(output, dim=1)
+            
+            total_loss += loss.item()
+            correct_samples += pred.eq(target).sum()
+
+    avg_loss = total_loss / total_samples
+    loss_history.append(avg_loss)
+    print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
+          '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
+          '{:5}'.format(total_samples) + ' (' +
+          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
+
+
+# In[8]:
+
+
+N_EPOCHS = 25
+
+start_time = time.time()
+model = ViT(image_size=28, patch_size=7, num_classes=10, channels=1,
+            dim=64, depth=6, heads=8, mlp_dim=128)
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+
+train_loss_history, test_loss_history = [], []
+for epoch in range(1, N_EPOCHS + 1):
+    print('Epoch:', epoch)
+    train_epoch(model, optimizer, train_loader, train_loss_history)
+    evaluate(model, test_loader, test_loss_history)
+
+print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
+
+
+# ## Custom GNN Transformer architecture 
+
+# In[2]:
+
+
+# Out implementation of GNN Transformer
+import torch
+import torch.nn as nn
+
+
+class GNNTransformer(torch.nn.Module):
+    def __init__(self):
+        super(GNNStack, self).__init__()
+        # TODO: Implement GNNTransformer stack
+        pass
+
+    def forward(self, data):
+        # TODO: Implement forward pass for GNNTransformer
+        pass
+
+    def loss(self, pred, label):
+        # TODO: Implement loss function
+        pass
+
+
+# In[3]:
+
+
+from scipy import linalg
+
+A = [1, 2]
+B = [[3, 4], [5, 6]]
+
+C = linalg.block_diag(A, B)
+
+print(A)
+print(B)
+print(C)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
